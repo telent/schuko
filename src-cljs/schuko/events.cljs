609151ec9b145769f0b3af0804ec73528b0528f8
Daniel Barlow <dan@telent.net>
2013-09-27 14:23:32 +0100

    first cut at a "wipe" transition, needs work to remove flicker and erroneous second call to removeChild

--- src-cljs/schuko/events.cljs
+++ src-cljs/schuko/events.cljs
@@ -23,14 +23,52 @@
     (set! (.-innerHTML el) (.-innerHTML (by-id :everything)))
     (seq (.querySelectorAll el "div.slide"))))
 
-(defn swap-nodes [front back effect]
+(defn swap-nodes [front back effect & params]
+  ;; XXX this wants to be a multimethod, not a huge great case statement
   (case effect
     :fade
     (do
       (set! (-> front .-style .-transition) "opacity 1s linear 500ms")
       (set! (-> back .-style .-transition) "opacity 1s ease")
       (set! (-> back .-style .-opacity) "0")
-      (set! (-> front .-style .-opacity) "1")))
+      (set! (-> front .-style .-opacity) "1"))
+    :wipe
+    ;; put a solid div in the top left corner in front of picture,
+    ;; grow it to cover the whole div, swap front and back, shrink
+    (let [curtain (.createElement js/document "div")
+          parent (.-parentNode front)]
+      (set! (.-className curtain) "wipe")
+      (.appendChild parent curtain)
+      (set! (-> curtain .-style .-width) "0px")
+      (set! (-> curtain .-style .-height) "0px")
+      (set! (-> curtain .-style .-zIndex) "99")
+      (set! (-> curtain .-style .-width)
+            (max (.-clientWidth front) (.-clientWidth back)))
+      (set! (-> curtain .-style .-height)
+            (max (.-clientHeight front) (.-clientHeight back)))
+      (.addEventListener
+       curtain "transitionend"
+       (fn [e]
+         ;; this gets called four times: when the height reaches max,
+         ;; when the width reaches max, when the height reaches 0,
+         ;; when the width reaches 0
+         (.log js/console  (pr-str [(.-propertyName e)
+                                    (.-clientWidth curtain)
+                                    (.-clientHeight curtain)]))
+
+         (cond
+          (and (= (.-propertyName e) "height") (> (.-clientHeight curtain) 0))
+          (do
+            (set! (-> front .-style .-opacity) "1")
+            (set! (-> back .-style .-opacity) "0")
+            (set! (-> curtain .-style .-height) "0px"))
+
+          (and (= (.-propertyName e) "width") (> (.-clientWidth curtain) 0))
+          (set! (-> curtain .-style .-width) "0px")
+
+          (and (= (.-clientHeight curtain) 0) (= (.-clientWidth curtain) 0))
+          (.removeChild parent curtain))
+         ))))
   (set! (.-className front) "front")
   (set! (.-className back) "back"))
 
@@ -42,7 +80,7 @@
          back (first (by-selector "div#content .back"))]
      (.log js/console (pr-str "showing slide " state front back))
      (set! (.-innerHTML back) (.-innerHTML (nth @all-slides state)))
-     (swap-nodes back front :fade)
+     (swap-nodes back front :wipe)
      )))
 
 (defn key-handler [e]
